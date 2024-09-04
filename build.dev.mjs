b2b647629a113cd3dcc9bf9631e5b3028a15c92b
Matej Suta <suta.matej@gmail.com>
2024-09-04 21:11:47 +0200

    removed todo

--- build.dev.mjs
+++ build.dev.mjs
@@ -18,7 +18,6 @@ let { host, port } = await buildContext.serve({
 console.log(`Serving the app at ${host}:${port}`)
 
 const typecheck = spawn('npx',
-  // TODO not sure if these flags should be moved to tsconfig file or stay here
   ['tsc', '--watch', '--pretty', '--noEmit', '--preserveWatchOutput'], {
   cwd: './src',
   stdio: 'pipe',
