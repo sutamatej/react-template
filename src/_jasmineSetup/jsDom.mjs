6c7bc5804f8f119ce56e750fed55fd0c7e1255ca
Matej Suta <suta.matej@gmail.com>
2024-09-19 20:29:49 +0200

    updated dependencies

--- src/_jasmineSetup/jsDom.mjs
+++ src/_jasmineSetup/jsDom.mjs
@@ -3,4 +3,4 @@ import { JSDOM } from 'jsdom'
 const dom = new JSDOM('<html><body></body></html>')
 global.document = dom.window.document
 global.window = dom.window
-// global.navigator = dom.window.navigator
+global.navigator = dom.window.navigator
