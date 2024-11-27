6af2992d4faabd829e9b3cccbeeb2b92919d5692
Matej Suta <suta.matej@gmail.com>
2024-11-27 11:47:13 +0100

    some updates

--- src/_jasmineSetup/jsDom.mjs
+++ src/_jasmineSetup/jsDom.mjs
@@ -3,4 +3,9 @@ import { JSDOM } from 'jsdom'
 const dom = new JSDOM('<html><body></body></html>')
 global.document = dom.window.document
 global.window = dom.window
-global.navigator = dom.window.navigator
+// global.navigator = dom.window.navigator
+
+Object.defineProperty(global, 'navigator', {
+  writable: true,
+  value: dom.window.navigator
+})
